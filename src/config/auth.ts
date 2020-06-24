000004867db29a408bfb8ef0f26b6df97d3b0fe6
Victor França <victorfclima@gmail.com>
2020-06-24 16:37:08 -0300

    New: Back-end started

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '5326aa855f631f90915dbb817f860faf',
+    expiresIn: '1d',
+  },
+};
---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import 'reflect-metadata';
+import express from 'express';
+import routes from './routes';
+
+import './database';
+
+const app = express();
+const PORT = 3333;
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(PORT, () =>
+  console.log(`Server is running at: http://localhost:${PORT}`),
+);
