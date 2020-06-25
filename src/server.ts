6fa5592484f220aa425eb1eac45c21b2a176ed15
Victor França <victorfclima@gmail.com>
2020-06-24 21:48:59 -0300

    New: Add global excepcional handling

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import 'reflect-metadata';
-import express from 'express';
+
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+
 import routes from './routes';
 import uploadConfig from './config/upload';
+import AppError from './errors/AppError';
 
 import './database';
 
@@ -11,6 +15,20 @@ const PORT = 3333;
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+  console.error(err);
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(PORT, () =>
   console.log(`Server is running at: http://localhost:${PORT}`),
