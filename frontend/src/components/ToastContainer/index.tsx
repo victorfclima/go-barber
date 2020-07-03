aff33a2f6a32925e832365bd7e95c4c13ceea3a4
Victor França <victorfclima@gmail.com>
2020-07-03 15:54:04 -0300

    New: Add effects on toast messages

--- frontend/src/components/ToastContainer/index.tsx
+++ frontend/src/components/ToastContainer/index.tsx
@@ -1,20 +1,31 @@
 import React from 'react';
+import { useTransition } from 'react-spring';
+
+import Toast from './Toast/index';
 
 import { Container } from './styles';
 
 import { ToastMessage } from '../../hooks/Toast';
-
-import Toast from './Toast/index';
 
 interface ToastContainerProps {
   messages: ToastMessage[];
 }
 
 const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
+  const messagesWithTransitions = useTransition(
+    messages,
+    message => message.id,
+    {
+      from: { right: '-120%', opacity: 0 },
+      enter: { right: '0%', opacity: 1 },
+      leave: { right: '-120%', opacity: 0 },
+    },
+  );
+
   return (
     <Container>
-      {messages.map(message => {
-        return <Toast key={message.id} message={message} />;
+      {messagesWithTransitions.map(({ item, key, props }) => {
+        return <Toast key={key} style={props} message={item} />;
       })}
     </Container>
   );
